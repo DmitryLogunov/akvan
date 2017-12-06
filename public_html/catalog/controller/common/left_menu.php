55c1cc12760336e562470e88d4c86f54d01ac2c1
Dmitry Logunov <dmitry.logunov@hotmail.com>
2017-12-06 23:18:08 +0300

    left menu
    left menu href

--- public_html/catalog/controller/common/left_menu.php
+++ public_html/catalog/controller/common/left_menu.php
@@ -3,6 +3,28 @@ class ControllerCommonLeftMenu extends Controller {
 	public function index() {
 		// Left menu
         $data = array();
-		return $this->load->view('common/left_menu', $data);
+        $this->load->model('catalog/category');
+        $categories = $this->model_catalog_category->getCategories();
+
+        $data['categories'] = array();
+        foreach ($categories as $category) {
+          $children = $this->model_catalog_category->getCategories($category['category_id']);
+
+          if(count($children) > 0) {
+            foreach ($children as $child) {
+              $child['children'] = $this->model_catalog_category->getCategories($child['category_id']);
+              $child['href'] = (count($child['children']) > 0) ?
+                               'javascript:void(0)' :
+                               '/index.php?route=product/category&path='.$child['category_id'];
+              $category['children'][] = $child;
+            }
+            $category['href'] = 'javascript:void(0)';
+          } else {
+            $category['href'] = '/index.php?route=product/category&path='.$category['category_id'];
+          }
+          $data['categories'][] = $category;
+        }
+
+        return $this->load->view('common/left_menu', $data);
 	}
 }
